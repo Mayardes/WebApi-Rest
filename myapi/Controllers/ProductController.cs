e90fa44743967fb2e509cd5e96b54f6e68e7cbb4
mayardes <mayardesoliveira@gmail.com>
2021-01-24 15:47:14 -0400

    including controller product with get - getbyid- getbycategory

---
+++ myapi/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myapi.Data;
+using myapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myapi.Controllers
+{
+    [Route("product")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var product = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return Ok(product);
+        }
+    }
+}
